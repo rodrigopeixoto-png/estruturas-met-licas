864e179ce923926512309f234d5d02418a5ca934
rodrigopeixoto-png <rodrigo.peixoto@sesp.pr.gov.br>
2026-07-31 17:18:51 -0300

    Update solver.py

--- modules/solver.py
+++ modules/solver.py
@@ -4,11 +4,11 @@ class MotorCalculo3D:
     def __init__(self):
         """
         Motor de Análise Matricial 3D de Estruturas em Pure Python / NumPy.
-        Módulo de Elasticidade (E) e Cisalhamento (G) para Aço NBR 8800.
+        Propriedades mecânicas e geométricas baseadas na NBR 8800.
         """
         self.E = 200e6      # 200 GPa = 200.000.000 kN/m²
         self.G = 77e6       # 77 GPa = 77.000.000 kN/m²
-        self.A = 0.005      # Área da Seção (m²) ~ Profilada/I
+        self.A = 0.005      # Área da Seção (m²)
         self.Iy = 0.0001    # Momento de Inércia Y (m⁴)
         self.Iz = 0.0001    # Momento de Inércia Z (m⁴)
         self.J = 0.00005    # Inércia à Torção (m⁴)
@@ -42,13 +42,12 @@ class MotorCalculo3D:
         L = np.sqrt(dx**2 + dy**2 + dz**2)
         if L == 0: L = 1e-6
 
-        # Cossenos diretores
         cx, cy, cz = dx/L, dy/L, dz/L
         
-        # Rigidez Local (12x12)
+        # Matriz Local (12x12)
         k_loc = np.zeros((12, 12))
         
-        # Esforço Axial
+        # Axial
         EA_L = (self.E * self.A) / L
         k_loc[0, 0] = k_loc[6, 6] = EA_L
         k_loc[0, 6] = k_loc[6, 0] = -EA_L
@@ -58,37 +57,37 @@ class MotorCalculo3D:
         k_loc[3, 3] = k_loc[9, 9] = GJ_L
         k_loc[3, 9] = k_loc[9, 3] = -GJ_L
         
-        # Flexão Z (Cortante Y)
-        12EIz_L3 = (12 * self.E * self.Iz) / (L**3)
-        6EIz_L2 = (6 * self.E * self.Iz) / (L**2)
-        4EIz_L = (4 * self.E * self.Iz) / L
-        2EIz_L = (2 * self.E * self.Iz) / L
+        # Flexão Z (Cortante Y) - Variáveis corrigidas
+        k12_Iz = (12 * self.E * self.Iz) / (L**3)
+        k6_Iz  = (6 * self.E * self.Iz) / (L**2)
+        k4_Iz  = (4 * self.E * self.Iz) / L
+        k2_Iz  = (2 * self.E * self.Iz) / L
         
-        k_loc[1, 1] = k_loc[7, 7] = 12EIz_L3
-        k_loc[1, 7] = k_loc[7, 1] = -12EIz_L3
-        k_loc[1, 5] = k_loc[5, 1] = k_loc[1, 11] = k_loc[11, 1] = 6EIz_L2
-        k_loc[5, 7] = k_loc[7, 5] = -6EIz_L2
-        k_loc[7, 11] = k_loc[11, 7] = -6EIz_L2
-        k_loc[5, 5] = k_loc[11, 11] = 4EIz_L
-        k_loc[5, 11] = k_loc[11, 5] = 2EIz_L
+        k_loc[1, 1] = k_loc[7, 7] = k12_Iz
+        k_loc[1, 7] = k_loc[7, 1] = -k12_Iz
+        k_loc[1, 5] = k_loc[5, 1] = k_loc[1, 11] = k_loc[11, 1] = k6_Iz
+        k_loc[5, 7] = k_loc[7, 5] = -k6_Iz
+        k_loc[7, 11] = k_loc[11, 7] = -k6_Iz
+        k_loc[5, 5] = k_loc[11, 11] = k4_Iz
+        k_loc[5, 11] = k_loc[11, 5] = k2_Iz
 
-        # Flexão Y (Cortante Z)
-        12EIy_L3 = (12 * self.E * self.Iy) / (L**3)
-        6EIy_L2 = (6 * self.E * self.Iy) / (L**2)
-        4EIy_L = (4 * self.E * self.Iy) / L
-        2EIy_L = (2 * self.E * self.Iy) / L
+        # Flexão Y (Cortante Z) - Variáveis corrigidas
+        k12_Iy = (12 * self.E * self.Iy) / (L**3)
+        k6_Iy  = (6 * self.E * self.Iy) / (L**2)
+        k4_Iy  = (4 * self.E * self.Iy) / L
+        k2_Iy  = (2 * self.E * self.Iy) / L
 
-        k_loc[2, 2] = k_loc[8, 8] = 12EIy_L3
-        k_loc[2, 8] = k_loc[8, 2] = -12EIy_L3
-        k_loc[2, 4] = k_loc[4, 2] = k_loc[2, 10] = k_loc[10, 2] = -6EIy_L2
-        k_loc[4, 8] = k_loc[8, 4] = 6EIy_L2
-        k_loc[8, 10] = k_loc[10, 8] = 6EIy_L2
-        k_loc[4, 4] = k_loc[10, 10] = 4EIy_L
-        k_loc[4, 10] = k_loc[10, 4] = 2EIy_L
+        k_loc[2, 2] = k_loc[8, 8] = k12_Iy
+        k_loc[2, 8] = k_loc[8, 2] = -k12_Iy
+        k_loc[2, 4] = k_loc[4, 2] = k_loc[2, 10] = k_loc[10, 2] = -k6_Iy
+        k_loc[4, 8] = k_loc[8, 4] = k6_Iy
+        k_loc[8, 10] = k_loc[10, 8] = k6_Iy
+        k_loc[4, 4] = k_loc[10, 10] = k4_Iy
+        k_loc[4, 10] = k_loc[10, 4] = k2_Iy
 
         # Matriz de Rotação 3D (3x3)
         D = np.sqrt(cx**2 + cy**2)
-        if D < 1e-5: # Elemento Vertical
+        if D < 1e-5:
             r3x3 = np.array([[0, 0, cz], [0, 1, 0], [-cz, 0, 0]])
         else:
             r3x3 = np.array([
@@ -97,12 +96,10 @@ class MotorCalculo3D:
                 [-cy/D, cx/D, 0]
             ])
             
-        # Matriz de Transformação Global (12x12)
         T = np.zeros((12, 12))
         for b in range(4):
             T[b*3:(b+1)*3, b*3:(b+1)*3] = r3x3
             
-        # K_global = T_transposta * K_local * T
         k_glob = T.T @ k_loc @ T
         return k_glob, L
 
@@ -114,7 +111,7 @@ class MotorCalculo3D:
         
         for n1, n2 in self.barras:
             z1, z2 = self.nos[n1][2], self.nos[n2][2]
-            if (z1 + z2) / 2.0 > 0.1: # Barra de cobertura
+            if (z1 + z2) / 2.0 > 0.1:
                 x1, y1, _ = self.nos[n1]
                 x2, y2, _ = self.nos[n2]
                 L = np.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
@@ -122,7 +119,6 @@ class MotorCalculo3D:
                 cargas_equiv_z = -q_linear * L / 2.0
                 momento_equiv = -q_linear * (L**2) / 12.0
                 
-                # Aplicação nos nós do elemento
                 self.cargas_nodais[n1*6 + 2] += cargas_equiv_z
                 self.cargas_nodais[n2*6 + 2] += cargas_equiv_z
                 self.cargas_nodais[n1*6 + 4] += momento_equiv
@@ -134,10 +130,9 @@ class MotorCalculo3D:
             num_dofs = len(self.nos) * 6
             K_global = np.zeros((num_dofs, num_dofs))
             
-            # 1. Montagem da Matriz de Rigidez Global
             comprimentos = []
             for n1, n2 in self.barras:
-                k_elem, L = self.matriz_elemento_3d(n1, n2)
+                k_elem, L = self._matriz_elemento_3d(n1, n2)
                 comprimentos.append(L)
                 
                 dofs = [n1*6 + d for d in range(6)] + [n2*6 + d for d in range(6)]
@@ -145,22 +140,18 @@ class MotorCalculo3D:
                     for j in range(12):
                         K_global[dofs[i], dofs[j]] += k_elem[i, j]
 
-            # 2. Aplicação das Condições de Contorno (Graus Livres)
             dofs_livres = [d for d in range(num_dofs) if d not in self.apoios]
             
             K_livre = K_global[np.ix_(dofs_livres, dofs_livres)]
             F_livre = self.cargas_nodais[dofs_livres]
 
-            # 3. Solução do Sistema de Equações
             U_livre = np.linalg.solve(K_livre, F_livre)
             
             U_completo = np.zeros(num_dofs)
             U_completo[dofs_livres] = U_livre
 
-            # 4. Cálculo dos Esforços Solicitantes Máximos
             desloc_max_mm = np.max(np.abs(U_completo)) * 1000.0
             
-            # Estimativa conservadora dos esforços com base nos deslocamentos e cargas nodais
             q_max = np.max(np.abs(self.cargas_nodais)) if len(self.cargas_nodais) > 0 else 1.0
             L_medio = np.mean(comprimentos) if len(comprimentos) > 0 else 5.0
             
@@ -180,7 +171,3 @@ class MotorCalculo3D:
 
         except Exception as e:
             return {"sucesso": False, "erro": str(e)}
-
-    # Alias interno para retrocompatibilidade
-    def matriz_elemento_3d(self, n1, n2):
-        return self._matriz_elemento_3d(n1, n2)
